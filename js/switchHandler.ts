40014443f94eba6fb4a9779a1da8e5aec502da97
Camilla Gretsch <camilla.gretschg@bluewin.ch>
2023-05-05 16:17:23 +0200

    sitch clickable

--- js/switchHandler.ts
+++ js/switchHandler.ts
@@ -1,6 +1,6 @@
 class SwitchHandler {
 
-    switchStates = {
+    private switchStates = {
         "switch1": false,
         "switch2": false,
         "switch3": false,
@@ -9,22 +9,35 @@ class SwitchHandler {
         "switch6": false,
         "switch7": false
     }
+
     constructor() {
 
     }
     
     switchClicked(e) {
+        if (e.target.src.includes("switch-on")) {
+            e.target.src = "icons/switch-off.png";
+        } else {
+            e.target.src = "icons/switch-on.png";
+        }
+
         switch (e.target.id) {
             case "switch1":
                 this.switchStates.switch1 = !this.switchStates.switch1;
             case "switch2":
                 this.switchStates.switch2 = !this.switchStates.switch2;
-        }
-        console.log(this.switchStates)
-        if (e.target.src.includes("switch-on")) {
-            e.target.src = "icons/switch-off.png"
-        } else {
-            e.target.src = "icons/switch-on.png"
+            case "switch3":
+                this.switchStates.switch3 = !this.switchStates.switch3;
+            case "switch4":
+                this.switchStates.switch4 = !this.switchStates.switch4;
+            case "switch5":
+                this.switchStates.switch5 = !this.switchStates.switch5;
+            case "switch6":
+                this.switchStates.switch6 = !this.switchStates.switch6;
+            case "switch7":
+                this.switchStates.switch7 = !this.switchStates.switch7;
         }
     }   
 }
+
+let handler = new SwitchHandler()
