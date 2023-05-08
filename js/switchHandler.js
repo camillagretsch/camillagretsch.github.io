c8a991b68cbe2359bc6cf659e266a4267aaacbfe
Camilla Gretsch <camilla.gretschg@bluewin.ch>
2023-05-08 23:19:21 +0200

    add countdown

--- js/switchHandler.js
+++ js/switchHandler.js
@@ -13,10 +13,11 @@ class SwitchHandler {
 
     constructor() {
         this.bombeImage = document.getElementById("bombe");
-        this.countdown( "countdown", 0, 5 );
+        countdown( "countdown", 0, 20 );
     }    
 
     switchClicked(e) {
+        this.bombeImage.classList.remove("inner");
         if (e.target.src.includes("switch-off")) {
             e.target.src = "icons/switch-on.png"
         } else {
@@ -58,36 +59,49 @@ class SwitchHandler {
             this.switchStates.switch6 == false &&
             this.switchStates.switch7 == false) {
                 this.bombeImage.src = "icons/bombe2.png"
+                document.getElementById("countdown").style.display = "none"
+                disableSwitch()
         } else {
             this.bombeImage.src = "icons/bombe.png"
         }
     }
-
-    countdown( elementName, minutes, seconds ) {
-        var element, endTime, hours, mins, msLeft, time;
-        
-        function twoDigits( n ) {
-            return (n <= 9 ? "0" + n : n);
-        }
-        
-        function updateTimer() {
-            msLeft = endTime - (+new Date);
-            if ( msLeft < 1000 ) {
-                // element.innerHTML = "Time is up!";
-                document.getElementById("bombe").src = "icons/explode.png"
-            } else {
-                time = new Date( msLeft );
-                hours = time.getUTCHours();
-                mins = time.getUTCMinutes();
-                element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-                setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
-            }
-        }
-
-        element = document.getElementById( elementName );
-        endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-        updateTimer();
-    }
 }
 
 let handler = new SwitchHandler();
+
+function countdown( elementName, minutes, seconds ) {
+    var element, endTime, hours, mins, msLeft, time;
+    
+    function twoDigits( n ) {
+        return (n <= 9 ? "0" + n : n);
+    }
+    
+    function updateTimer() {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            element.innerHTML = "0:00";
+            document.getElementById("bombe").src = "icons/explode.png"
+            disableSwitch()
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+function disableSwitch() {
+    document.getElementById("switch1").disabled = true;
+    document.getElementById("switch2").disabled = true;
+    document.getElementById("switch3").disabled = true;
+    document.getElementById("switch4").disabled = true;
+    document.getElementById("switch5").disabled = true;
+    document.getElementById("switch6").disabled = true;
+    document.getElementById("switch7").disabled = true;
+}
